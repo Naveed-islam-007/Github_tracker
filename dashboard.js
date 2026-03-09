0d58d86017c9104925f02a9e050e91743b352e97
Naveed-islam-007 <islamnaveed631@gmail.com>
2026-03-09 17:27:09 +0600

    sixth

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,24 @@
 const countElement = document.getElementById('count');
 const apiContainer = document.getElementById('api-container');
+const cardModal=document.getElementById('card');
+const pdetails=document.getElementById('pdetails');
+const author=document.getElementById('author');
+const date=document.getElementById('date');
+const description=document.getElementById('description');
+const nme=document.getElementById('name');
+const priority=document.getElementById('priority');
+const allbtn = document.getElementById('allbtn');
+const openbtn = document.getElementById('openbtn');
+const closedbtn = document.getElementById('closedbtn');
+const label1 = document.getElementById('label1');
+const label2 = document.getElementById('label2');
+
+
+
 
 const loadprojects = async () => {
     const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
-
+   let allprojects=[];
     const res = await fetch(url);
     const data = await res.json();
     allprojects=data.data;
@@ -19,15 +34,12 @@ const displayProjects = (projects) => {
 
         const div = document.createElement('div');
         div.className = 'p-1 rounded-lg shadow-lg mb-5';
-
-        div.innerHTML = `
-        <div class="card-body flex flex-col gap-2">
+         div.innerHTML = `
+        <div class="card-body flex gap-2">
 
             <div class="flex justify-between items-center">
-                <img src="${project.status.toLowerCase() === 'open' ? './assets/Open-Status.png' : './assets/Closed-Status.png'}">
-
-                ${
-                    project.priority === 'high'
+                <img src="${project.status.toLowerCase()==='open' ? './assets/Open-Status.png' : './assets/Closed-Status.png'}">
+                 ${  project.priority === 'high'
                     ? '<div class="badge badge-soft badge-success">High</div>'
                     : project.priority === 'medium'
                     ? '<div class="badge badge-soft badge-warning">Medium</div>'
@@ -35,7 +47,7 @@ const displayProjects = (projects) => {
                 }
             </div>
 
-            <h2 class="text-lg font-bold">${project.title}</h2>
+            <h2 onclick="toggleModal(${project.id})" class="text-lg font-bold">${project.title}</h2>
 
             <p>${project.description}</p>
 
@@ -96,5 +108,44 @@ const selectcategory = (type) => {
 
     displayProjects(filteredProjects);
 };
+
+async function toggleModal(id) {
+   const url=`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+   const res=await fetch(url);
+   const project=await res.json();
+   const detail=project.data;
+    pdetails.innerText=`#${detail.id} ${detail.title}`;
+    author.innerText=`Opened by ${detail.author}`;
+    date.innerText=`${new Date(detail.createdAt).toLocaleDateString()}`;
+    label1.innerText=`${detail.labels[0]}`;
+   label1.className =
+    detail.labels[0].toLowerCase() === 'bug'
+        ? 'badge badge-error'
+        : detail.labels[0].toLowerCase() === 'help wanted'
+        ? 'badge badge-warning'
+        : detail.labels[0].toLowerCase() === 'enhancement'
+        ? 'badge badge-success'
+        : 'badge badge-info';
+    label2.innerText=`${detail.labels[1]}`;
+    label2.className =
+    detail.labels[1].toLowerCase() === 'bug'
+        ? 'badge badge-error'
+        : detail.labels[1].toLowerCase() === 'help wanted'
+        ? 'badge badge-warning'
+        : detail.labels[1].toLowerCase() === 'enhancement'
+        ? 'badge badge-success'
+        : 'badge badge-info';
+   
+    description.innerText=`${detail.description}`;
+    nme.innerText = detail.assignee || "Unassigned";
+    priority.innerText= `${detail.priority}`;
+   
+    cardModal.showModal();
+            }
+
+
+function closeModal() {
+    cardModal.close();
+}
 
 loadprojects();
